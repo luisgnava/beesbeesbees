16735fe4085e4e93e1986a27c39bf13c74416a49
luisgnava <luis.gnava85@gmail.com>
2018-01-15 18:34:06 -0600

    Finished grub with all its components

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
-var Grub = function() {
-};
+//Create a Grub class, in pseudoclassical style, with:
+//an age property that is set to 0
+//a color property that is set to pink
+//a food property that is set to jelly
+//an eat method
 
+var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.eat = 'jelly'
+};
+Grub.prototype.eat = function() {}
