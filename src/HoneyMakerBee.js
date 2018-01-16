e64d3af9212d1f751fba4fee99c15fe753d8deda
luisgnava <luis.gnava85@gmail.com>
2018-01-15 19:02:07 -0600

    Started HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
-
+HoneyMakerBee.prototype.eat = function() {};
+HoneyMakerBee.prototype.makeHoney = function() {
+  if (this.honeyPot == 0) {
+    this.honeyPot++;
+  }
+  HoneyMakerBee.prototype.giveHoney = function() {
+    if (this.honeyPot > 3) {
+      this.honeyPot--;
+    }
+    return makeHoney;
+    return giveHoney;
+  }
+};
