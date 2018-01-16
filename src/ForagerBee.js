257197bb473a92d47fae6c9b17ee6fc3134851ad
luisgnava <luis.gnava85@gmail.com>
2018-01-15 19:21:39 -0600

    Started ForagerBee.js, added initial lines of code

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.color = 'find pollen';
+  this.food = 'jelly';
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
 };
+foragerBee.prototype.eat = function() {};
+//function should add a value inside treasure chest array
+foragerBee.prototype.forage = function() {
 
+
+};
